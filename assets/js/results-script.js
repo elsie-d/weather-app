02ab091c7651853d765294020afe38a6ef78f0dd
Elsie <elsie.dh@gmail.com>
2023-10-26 00:02:20 -0700

    added event listner in script.js and created resluts js file

---
+++ assets/js/results-script.js
@@ -0,0 +1,3 @@
+var queryURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey;
+var city;
+var apiKey = 'd2049667069ea0037ba25d8371e6e8cc'
---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+
+var searchForm = document.querySelector('form')
+
+function handleSearch(event){
+event.preventDefault();
+var searchInput = document.querySelector('#city-search')   
+
+    if (!searchInput) {
+        alert('Cannot search for blank input');
+}
+
+var queryString = './search-return.html?q=' + searchInput;
+location.assign(queryString);
+
+fetch(queryURL);
+
+console.log(queryString);
+}
+
+searchForm.addEventListener('submit', handleSearch)
+ 
+
+console.log("test") 
