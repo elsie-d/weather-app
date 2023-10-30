1025cfefb25790c8910d201ea24754935e4966bc
Elsie <elsie.dh@gmail.com>
2023-10-29 21:38:39 -0700

    moved all functionality to 1 page (results page)

--- assets/js/results-script.js
+++ assets/js/results-script.js
@@ -1,23 +1,21 @@
-var searchParams = new URLSearchParams(window.location.search);
-//console.log(searchParams.has('q'));
-//console.log(searchParams.get('q'));
-var cityParam = searchParams.get('q').trim();
-console.log(cityParam)
-
-//var queryURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityParam + '&appid=d2049667069ea0037ba25d8371e6e8cc'
-//console.log(queryURL)
-//var apiKey = 'd2049667069ea0037ba25d8371e6e8cc'  <-- 401 error when variable is used in url string builder
+var citySearch = document.querySelector("#city-search");
+var searchBtn = document.querySelector("#search-btn");
 var currentWeatherDiv = document.querySelector(".current-weather")
 var forecastCardsDiv = document.querySelector(".forecast-cards")
+var apiKEY = 'd2049667069ea0037ba25d8371e6e8cc'
+
+
+
+
 
 // Updates webpage with weather for city that was queried
 
-var createForecastCard = (cityParam, weatherBlock, index) => {
+var createForecastCard = (cityValue, weatherBlock, index) => {
     if (index === 0){
         return `
          <div class="details">
-          <h2>${cityParam}</h2>
-          <h4>Temperature: ${(((weatherBlock.main.temp - 273.15)*1.8)+32).toFixed(2)} ℉</h4>
+          <h2>${cityValue}</h2>
+          <h4>Temperature: ${(((weatherBlock.main.temp - 273.15)*1.8)+32).toFixed(0)} ℉</h4>
           <h4>Humidity:  ${weatherBlock.main.humidity}%</h4>
           <h4>Wind: ${weatherBlock.wind.speed} M/S</h4>
       </div>
@@ -32,23 +30,28 @@ var createForecastCard = (cityParam, weatherBlock, index) => {
                 <p class="card-text">${weatherBlock.dt_txt.split(" ")[0]}</p>
               </div>
               <ul class="list-group list-group-flush">
-                <li class="list-group-item">Temp: ${(((weatherBlock.main.temp - 273.15)*1.8)+32).toFixed(2)} ℉</li>
+                <li class="list-group-item">Temp: ${(((weatherBlock.main.temp - 273.15)*1.8)+32).toFixed(0)} ℉</li>
                 <li class="list-group-item">Humidity: ${weatherBlock.main.humidity}%</li>
                 <li class="list-group-item">Wind: ${weatherBlock.wind.speed} M/S</li>
               </ul>
             </div>` }
 }
 
-console.log(createForecastCard)
+
+
+
+
+
+
 
 
 // Use Coodintes to pull current weather and forecast info
 
-var getWeahterInfo = (cityParam, lat, lon) => {
-    var forecast_API_URL = 'http://api.openweathermap.org/data/2.5/forecast?lat=' +lat + '&lon=' + lon + '&appid=d2049667069ea0037ba25d8371e6e8cc'
+var getWeahterInfo = (cityValue, lat, lon) => {
+    var forecast_API_URL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKEY}`
     
     fetch(forecast_API_URL).then(res => res.json()).then(data => {
-        
+    console.log(data)      
 
         var uniqueDays = [];
         var fiveDayForecast = data.list.filter(forecast => {
@@ -58,7 +61,7 @@ var getWeahterInfo = (cityParam, lat, lon) => {
             }
         })
             
-            cityParam = "";
+            citySearch.value = "";
             currentWeatherDiv.innerHTML="";
             forecastCardsDiv.innerHTML="";
 
@@ -66,9 +69,9 @@ var getWeahterInfo = (cityParam, lat, lon) => {
             console.log(fiveDayForecast);
             fiveDayForecast.forEach((weatherBlock, index) => {
                 if (index === 0){
-                    currentWeatherDiv.insertAdjacentHTML("beforeend",createForecastCard(cityParam, weatherBlock, index)) ;
+                    currentWeatherDiv.insertAdjacentHTML("beforeend",createForecastCard(cityValue, weatherBlock, index)) ;
                 } else {
-                    forecastCardsDiv.insertAdjacentHTML("beforeend",createForecastCard(cityParam, weatherBlock, index)) ;
+                    forecastCardsDiv.insertAdjacentHTML("beforeend",createForecastCard(cityValue, weatherBlock, index)) ;
                 }
         })
 
@@ -78,28 +81,31 @@ var getWeahterInfo = (cityParam, lat, lon) => {
        });
 }
 
-//Convert City Pram Coordinates
 
-var getCityCoordinates = () => {
-   var geoCode_API_URL = 'http://api.openweathermap.org/geo/1.0/direct?q='+ cityParam + '&limit=5&appid=d2049667069ea0037ba25d8371e6e8cc'
+//Convert City Coordinates
 
-   fetch(geoCode_API_URL).then(res => res.json()).then(data => {
-    if(!data.length) return alert ('No coordinars found for' + cityParam)
-    console.log(data)
-    var {name, lat, lon} = data[0];
-    getWeahterInfo(name, lat, lon);
-   }).catch(() => {
-    alert('An error occurred while fetching coordinates!');
-   });
+var getCoordinates = () => {
+    var cityValue = citySearch.value.trim();
+    console.log('clicked');
+    console.log(cityValue);
+   // if(!cityValue)return;
+    var geoCode_API_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityValue}&limit=5&appid=${apiKEY}`
+
+    fetch(geoCode_API_URL).then(res => res.json()).then(data =>{
+        if(!data.length) return alert (`Having trouble locating location of:  ${cityValue}`)
+        var {name, lat, lon} = data[0]
+        getWeahterInfo(name, lat, lon);
+        console.log(data)
+    }).catch(() =>{
+        alert("An errror occured while fetching data")
+    })
 
 
 }
 
 
 
-
-$(document).ready(getCityCoordinates)
-
-
-
-
+searchBtn.addEventListener("click", function(e){
+    e.preventDefault()
+    getCoordinates()
+});
